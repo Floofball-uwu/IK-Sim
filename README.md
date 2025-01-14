8f0362f0042852b9c9d3ab6422bd44eaa087e0bb
Floofball <59265546+Floofball-uwu@users.noreply.github.com>
2025-01-14 12:30:39 +0100

    IK Algos library inserted, other stuff adjusted, work can begin

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 A simple simulation using threepp for developing inverse kinematics... from scratch... so it can work on microcontrollers like Arduino without the STL library... *sigh*
 
-## Assumptions within the lib
-- No access to the C++ Standard Library, because Arduino...
-  - Exceptions: common math functions like trigonometric ones and such.
-    - In such cases, a wrapper function will be made for easy replacement with an existing implementation. 
-- Children are never added or removed within a tree (C arrays are used), so odd behavior can be expected if this is done.
-- Exception handling? What exception handling?
+# Several libraries??
+The core inverse kinematics is based off *Inverse Kinematics Algorithms* by chFleschutz (a godsend), found [here](https://github.com/chFleschutz/inverse-kinematics-algorithms) and internally known as `ik-algorithms`.
 
-## TODO
-- math setup
-- namespaces (IkLib)
+Then there's the modified 3D counterpart named `ik-algorithms-3d`. The Standard Library may be utilized here for logic. 
+
+Lastly the goal is to make perhaps a more or less optimized, but fully STL-less version of the 3D one, known as `ik-lib`. This version is then intended to be used on microcontrollers.
+
+## Assumptions within the ik-lib
+- No access to the C++ Standard Library, because Arduino... or microcontrollers in general
+  - Exceptions: common math functions like trigonometric ones and such.
+    - In such cases, a wrapper function will be made for easy replacement with an existing implementation.
+- Exception handling? What exception handling?
--- main.cpp
+++ main.cpp
@@ -1,55 +1,32 @@
+#include "Skeleton.h"
 #include "threepp/threepp.hpp"
 
 #include "threepp/extras/imgui/ImguiContext.hpp"
 
-using namespace threepp;
-
-namespace {
-
-    auto createMesh() {
-        const auto geometry = BoxGeometry::create();
-        const auto material = MeshBasicMaterial::create();
-        material->color = Color::green;
-
-        auto mesh = Mesh::create(geometry, material);
-
-        return mesh;
-    }
-
-}
-
 int main() {
 
-    Canvas canvas;
-    GLRenderer renderer{canvas.size()};
+    threepp::Canvas canvas;
+    threepp::GLRenderer renderer{canvas.size()};
 
-    PerspectiveCamera camera(60, canvas.aspect(), 0.1, 1000);
+    threepp::PerspectiveCamera camera(60, canvas.aspect(), 0.1, 1000);
     camera.position.z = 5;
 
-    Scene scene;
-    scene.background = Color::aliceblue;
+    threepp::Scene scene;
+    scene.background = threepp::Color(0.5f, 0.5f, 0.5f);
 
-    auto mesh = createMesh();
-    scene.add(mesh);
-
-    bool& meshVisible = mesh->visible;
-
-    ImguiFunctionalContext ui(canvas.windowPtr(), [&meshVisible] {
+    ImguiFunctionalContext ui(canvas.windowPtr(), [&] {
         ImGui::SetNextWindowPos({}, 0, {});
                ImGui::SetNextWindowSize({230, 0}, 0);
                ImGui::Begin("Mesh settings");
-               ImGui::Checkbox("Visible", &meshVisible);
 
                ImGui::End();
     });
     // ui.makeDpiAware(); // to increase imgui size on high DPI screens
 
-    Clock clock;
-    float rotationSpeed = 0.5f;
+
+    threepp::Clock clock;
     canvas.animate([&] {
         const auto dt = clock.getDelta();
-
-        mesh->rotation.y += rotationSpeed * dt;
 
         renderer.render(scene, camera);
         ui.render();
